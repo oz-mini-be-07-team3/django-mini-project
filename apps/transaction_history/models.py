401cf1b72377d95de66187257906db52e2ff821b
BlueLight <dlrkdmf10@gmail.com>
2024-12-31 15:44:31 +0900

    🐛 오류 수정
    오류 수정

--- apps/transaction_history/models.py
+++ apps/transaction_history/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.common.models import CommonModel
+from apps.accounts.models import Account
 
 
-class TransactionHistory(models.Model):
-    account = models.ForeignKey('accounts.Account', on_delete=models.CASCADE) # 계좌 정보
+class TransactionHistory(CommonModel):
+    account = models.ForeignKey(Account, on_delete=models.CASCADE) # 계좌 정보
     post_transaction_amount = models.DecimalField(max_digits=18, decimal_places=2) # 거래 금액
     transaction_amount = models.DecimalField(max_digits=18, decimal_places=2) # 거래 후 잔액
     transaction_method = models.CharField(
@@ -22,5 +24,3 @@ class TransactionHistory(models.Model):
             ("WITHDRAW", "출금"),
         ]) # 입출금 타입
     transaction_timestamp = models.DateTimeField(db_index=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
