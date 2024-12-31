5acd051dcbc13815f3f31a38b06e309c2b5cacc7
SOLBI <thfqlsla35@gmail.com>
2024-12-31 12:12:03 +0900

    💡 chore: 오타수정

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.common.models import CommonModel
+from django.conf import settings
 
 BANK_CODES = [
     ("000", "알수없음"),
@@ -109,13 +110,13 @@ ACCOUNTS_TYPE = [
 
 # Create your models here.
 class Accounts(CommonModel):
-    user = models.ForeignKey('AUTH_USER_MODEL', on_delete=models.CASCADE) # user_id(FK)
+    user = models.ForeignKey('settings.AUTH_USER_MODEL', on_delete=models.CASCADE) # user_id(FK)
     bank_code = models.CharField(max_length=3, choices=BANK_CODES, default="000") # 기본적으로 000 "알수없음"선택됨
     account_number = models.CharField(max_length=30)
     account_type = models.CharField(max_length=30, choices=ACCOUNTS_TYPE, default="CHECKING") # 기본적으로 입출금 선택됨
     balance = models.IntegerField()
 
-def __str__(self):
-    return f"{self.get_bank_code_display()} - {self.account_number} ({self.get_account_type_display()})" # 국민은행 - 1234567890 (입출금)
+    def __str__(self):
+        return f"{self.get_bank_code_display()} - {self.account_number} ({self.get_account_type_display()})" # 국민은행 - 1234567890 (입출금)
 
 
