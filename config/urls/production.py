ee05ed5322aff88f6a3c2f6ce1eaae505676fd7f
hyo <umdoongs@gmail.com>
2025-01-02 16:58:10 +0900

    ✨ Feat: production code update
    feat: production code update

--- config/urls/production.py
+++ config/urls/production.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/users/', include('apps.users.urls')),
+    path('api/v1/accounts/', include('apps.accounts.urls')),
+    path("api/v1/transactions/", include("apps.transaction_history.urls")),
 ]
