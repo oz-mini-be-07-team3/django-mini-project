99c91a35880fed3714359569db0fc1525b7630cd
hyo <umdoongs@gmail.com>
2025-01-09 19:36:29 +0900

    ✨ Feat: create_user 테스트 추가
    Feat: create_user 테스트 추가

--- apps/users/tests.py
+++ apps/users/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UserTestCase(TestCase):
+
+	# 일반 유저 생성 테스트 함수
+	def test_create_user(self):
+		email = "asd@gmail.com"
+		password = "password123"
+
+		user = get_user_model().objects.create_user(email=email, password=password)
+
+		self.assertEqual(user.email, email)
+		self.assertTrue(user.check_password(password))
+		self.assertTrue(user.is_active)
+		self.assertFalse(user.is_staff)
+		self.assertFalse(user.is_admin)
+		self.assertFalse(user.is_superuser)
